a9294ebf369895a939cc4337959a7c48e6cd3bda
Denis Evers <denis-ev@users.noreply.github.com>
2022-06-08 23:14:11 +0800

    chore: update cloudflare-ip-whitelist-sync.sh

--- cloudflare-ip-whitelist-sync.sh
+++ cloudflare-ip-whitelist-sync.sh
@@ -21,3 +21,18 @@ echo "real_ip_header CF-Connecting-IP;" >> $CLOUDFLARE_FILE_PATH;
 
 #test configuration and reload nginx
 nginx -t && systemctl reload nginx
+
+firewall-cmd --permanent --delete-ipset=cfv4
+firewall-cmd --permanent --delete-ipset=cfv6
+firewall-cmd --permanent --new-ipset=cfv4 --type=hash:net
+firewall-cmd --permanent --new-ipset=cfv6 --type=hash:net --option=family=inet6
+
+for cidr in $(curl https://www.cloudflare.com/ips-v4); do \
+    firewall-cmd --permanent --ipset=cfv4 --add-entry=$cidr; \
+done
+
+for cidr in $(curl https://www.cloudflare.com/ips-v6); do \
+    firewall-cmd --permanent --ipset=cfv6 --add-entry=$cidr; \
+done
+
+firewall-cmd --reload
